e90cb21b86ca03d22fed50ab2bafcbf702725985
AllandoAlmeida <alandoalmeida0803@7j50z3.onmicrosoft.com>
2024-01-15 15:55:21 -0300

    fix: fix bug cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import * as express from 'express'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.enableCors()
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -47,7 +48,7 @@ async function bootstrap() {
   }
 
   app.use(redirectToSwagger)
-  /* app.enableCors() */
-  await app.listen(process.env.PORT)
+
+  await app.listen(3009)
 }
 bootstrap()
