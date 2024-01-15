f45ab74e944fcf36634f3c970b8e8da8fb539def
AllandoAlmeida <alandoalmeida0803@7j50z3.onmicrosoft.com>
2024-01-15 11:26:31 -0300

    fix: fix bug prisma.service

--- src/database/prisma.service.ts
+++ src/database/prisma.service.ts
@@ -4,12 +4,6 @@ import { PrismaClient } from '@prisma/client'
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-    try {
-      await this.$connect()
-      console.log('Connected to PrismaClient')
-    } catch (error) {
-      console.error('Error connecting to PrismaClient:', error)
-      process.exit(1) // Encerra a aplicação em caso de falha na conexão
-    }
+    await this.$connect()
   }
 }
